886da71ee09107e487c8a46846d8ff3a4b99dc11
Serhii Siedovolosyi <sedovolosiy@gmail.com>
2022-08-25 15:01:00 +0200

    fix error

--- main.rb
+++ main.rb
@@ -2,11 +2,22 @@
 
 require 'debug'
 
-def calc_position(line, o_pos, x_pos)
+def add_pos_steps(count_of_steps, idx, pos)
+  pos += idx
+  count_of_steps += 1
+  [pos, count_of_steps]
+end
+
+def calc_position(line, o_pos, x_pos, count_of_steps)
   line.each_with_index do |element, idx|
-    x_pos += idx if element == 'x'
-    o_pos += idx if element == 'o'
+    case element
+    when 'x'
+      x_pos, count_of_steps = add_pos_steps(count_of_steps, idx, x_pos)
+    when 'o'
+      o_pos, count_of_steps = add_pos_steps(count_of_steps, idx, o_pos)
+    end
   end
+  [x_pos, o_pos, count_of_steps]
 end
 
 def result(o_pos, x_pos)
@@ -26,19 +37,18 @@ def who_won?(list)
   list.each do |line|
     next if line.compact.empty?
 
-    count_of_steps += 1
-    calc_position(line, o_pos, x_pos)
+    x_pos, o_pos, count_of_steps = calc_position(line, o_pos, x_pos, count_of_steps)
   end
 
   result(o_pos, x_pos) if count_of_steps >= 3
 end
 
-_x_win_diagonal = [['x', 'o', nil], [nil, 'x', 'o'], [nil, nil, 'x']]
+x_win_diagonal = [['x', 'o', nil], [nil, 'x', 'o'], [nil, nil, 'x']]
 _x_win_horizontal = [%w[x x x], ['o', 'o', nil], [nil, nil, nil]]
 _o_win_vertical = [['x', nil, 'o'], [nil, 'x', 'o'], ['x', nil, 'o']]
 _nothing_win = [['x', 'o', nil],
                 %w[x x o],
                 %w[o x o]]
-x_one_step = [['x', nil, nil], [nil, nil, nil], [nil, nil, nil]]
+_x_one_step = [%w[x x o], [nil, nil, nil], [nil, nil, nil]]
 
-p who_won?(x_one_step)
+p who_won?(x_win_diagonal)
